803eb4392b0345916856e908fe61db770400da9c
SilviyaPBrayanova <silviya.brayanova@gmail.com>
2022-01-19 17:54:35 +0200

    control flow chapter

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Input error");
+
+    let number: i32  = buf.trim().parse().expect("");
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number % 4 == 0{
+        println!("{} is divisible by 4", number);
+    } else if number % 3 == 0{
+        println!("{} is divisible by 3", number);
+    } else if number % 2 == 0{
+        println!("{} is divisible by 2", number);
+    } else {
+        println!("{} is not divisible by 4, 3 or 2", number);
+    }
+
+    let condition = true;
+    let var = if condition {5} else {6};
+    println!("{}", var);
 }
