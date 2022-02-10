d73690492636e73082b927c88b090f684aadec23
SilviyaPBrayanova <silviya.brayanova@gmail.com>
2022-02-10 17:14:14 +0200

    Remove commented lines

--- restaurant/src/front_of_house.rs
+++ restaurant/src/front_of_house.rs
@@ -1,12 +1,10 @@
-//mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        pub fn seat_at_table() {}
-    }
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+    pub fn seat_at_table() {}
+}
 
-    pub mod serving {
-        fn take_order() {}
-        pub fn serve_order() {}
-        fn take_payment() {}
-    }
-//}
+pub mod serving {
+    fn take_order() {}
+    pub fn serve_order() {}
+    fn take_payment() {}
+}
